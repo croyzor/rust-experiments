430afdf1b66710b44ef439dfecec935d713bed63
Craig Roy <Craigr63@gmail.com>
2018-09-19 21:12:46 +0100

    Use a struct to represent the game state

--- fibby/src/main.rs
+++ fibby/src/main.rs
@@ -2,32 +2,62 @@ extern crate rand;
 
 use rand::Rng;
 
-fn print_board(board: &[[i8; 4]; 4]) {
-    println!("+---+---+---+---+");
-    for i in board.iter() {
-        // TODO: add padding for aligning with double-digit numbers
-        println!("| {} | {} | {} | {} |", i[0],i[1],i[2],i[3]);
-        println!("+---+---+---+---+");
-    }    
+struct Game {
+    board: Vec<Vec<Option<u8>>>,
+    score: u32,
 }
 
-fn count_empty(board: &[[i8; 4]; 4]) -> i8 {
-    let mut total: i8 = 0;
-    for row in board.iter() {
-        for elem in row {
-            if *elem == 0 {
-                total += 1;
-            }
+// TODO: Make this more generic
+fn print_row(row: &Vec<Option<u8>>) {
+    let tiles = row.iter().map(|&x| match x {
+        Some(i) => i.to_string(),
+        None => " ".to_string()
+    });
+    print!("| ");
+    for i in tiles {
+        print!("{}", i);
+        print!(" | ");
+    }
+    println!(" |");
+}
+
+impl Game {
+
+    fn new() -> Game {
+        Game {
+            board: vec![vec![None,None,None,None],
+                        vec![None,None,None,None],
+                        vec![None,None,None,None],
+                        vec![None,None,None,None]],
+            score: 0
         }
     }
-    return total;
+
+    fn print(&self) {
+        println!("+---+---+---+---+");
+        for i in self.board.iter() {
+            // TODO: add padding for aligning with double-digit numbers
+            print_row(i);
+            println!("+---+---+---+---+");
+        }
+    }
+
+    fn count_empty(&self) -> u8 {
+        let mut total: u8 = 0;
+        for row in self.board.iter() {
+            for elem in row.iter() {
+                match *elem {
+                    Some(_) => total += 1,
+                    None    => ()
+                };
+            }
+        }
+        return total;
+    }
 }
 
 fn main() {
-    let board = [[0,0,0,0],
-                 [0,0,0,0],
-                 [0,0,0,0],
-                 [0,0,0,0]];
-    print_board(&board);
-    println!("{}", count_empty(&board));
+    let game = Game::new();
+    game.print();
+    println!("{}", game.count_empty());
 }

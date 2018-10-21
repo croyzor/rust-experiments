e29227900510248bae588f6884a3bbf98d6caabb
Craig Roy <Craigr63@gmail.com>
2018-10-21 17:45:15 +0100

    Fix warnings in client

--- sock_commune/src/bin/client.rs
+++ sock_commune/src/bin/client.rs
@@ -1,9 +1,9 @@
 use std::net::UdpSocket;
 
 fn main() -> std::io::Result<()> {
-    let mut socket = UdpSocket::bind("127.0.0.1:12346")?;
+    let socket = UdpSocket::bind("127.0.0.1:12346")?;
     socket.connect("127.0.0.1:12345")?;
-    let mut msg = "hello".as_bytes();
+    let msg = "hello".as_bytes();
     socket.send(msg)?;
     Ok(())
 }

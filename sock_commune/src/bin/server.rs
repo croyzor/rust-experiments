476a1b30e993e0f29f6eaf9d06e45ee3e39c3bf4
Craig Roy <Craigr63@gmail.com>
2018-10-21 18:05:27 +0100

    Fix server warnings

--- sock_commune/src/bin/server.rs
+++ sock_commune/src/bin/server.rs
@@ -1,10 +1,10 @@
 use std::net::UdpSocket;
 
 fn main() -> std::io::Result<()> {
-    let mut socket = UdpSocket::bind("127.0.0.1:12345")?;
+    let socket = UdpSocket::bind("127.0.0.1:12345")?;
     // Buffer of 10 zeros
     let mut buf = [0; 10];
-    let (amt, src) = socket.recv_from(&mut buf)?;
+    socket.recv_from(&mut buf)?;
 
     let msg = String::from_utf8(buf.to_vec()).unwrap();
     println!("{}", msg);
